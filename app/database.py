5c4c5cb95ea971c193f79c5f62c69b9b2877bab0
vanmanhAI <156910105+vanmanhAI@users.noreply.github.com>
2024-12-04 23:24:17 +0700

    drop database

--- app/database.py
+++ app/database.py
@@ -1,24 +1,13 @@
-from pymongo import MongoClient
+from pymongo import MongoClient, DESCENDING, ASCENDING
 from datetime import datetime
 
 client = MongoClient('mongodb+srv://cvm:12345@twitter.7eviwvh.mongodb.net/')
 db = client['FIRE_DETECTION']  
-collection = db['fireNotifications']
 historyFireCollection = db['historyFire']   # Lịch sử các vụ cháy 
 logCollection = db['logs']  # Lịch sử các hoạt động của hệ thống
 
 # Tạo chỉ mục trên trường 'date' để sắp xếp nhanh
 historyFireCollection.create_index([("date", DESCENDING)])
-
-def save_data(smoke, fire, light, image):
-    data = {
-        'smoke': smoke,
-        'fire': fire,
-        'light': light,
-        'image': image,  # Có thể là đường dẫn tới ảnh hoặc dữ liệu ảnh
-        'date': datetime.now()
-    }
-    collection.insert_one(data)
     
     # LUA1, LUA2, LUA3, KHOI, %CHAY, %DETECT
 def save_history_fire_data(lua1, lua2, lua3, khoi, chay, detect):
